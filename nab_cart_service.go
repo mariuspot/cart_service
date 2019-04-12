e97b3a4832adefb145c2662fe0120aad3cc37e93
Marius <mariuspotgieter@gmail.com>
2019-04-12 06:41:59 +0200

    Added TLS encryption, including the test certs for grpc

--- nab_cart_service.go
+++ nab_cart_service.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/testdata"
 
 	// "google.golang.org/grpc/credentials"
 	// "google.golang.org/grpc/testdata"
@@ -21,7 +23,7 @@ import (
 )
 
 var (
-	// tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	tls      = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
 	certFile = flag.String("cert_file", "", "The TLS cert file")
 	keyFile  = flag.String("key_file", "", "The TLS key file")
 
@@ -31,28 +33,45 @@ var (
 	db_ip       = flag.String("db_ip", "127.0.0.1", "Database ip")
 	db_port     = flag.Int("db_port", 3306, "Database port")
 
+	metrics_ip   = flag.String("metric_ip", "127.0.0.1", "Metrics ip (prometheus)")
+	metrics_port = flag.Int("metric_port", 2112, "Metrics port (prometheus)")
+
 	ip   = flag.String("ip", "127.0.0.1", "The ip to listen on for the server")
 	port = flag.Int("port", 10000, "The port to listen on for the server")
 )
 
 func main() {
-
 	flag.Parse()
+
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *ip, *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
+	var opts []grpc.ServerOption
+	if *tls {
+		if *certFile == "" {
+			*certFile = testdata.Path("server1.pem")
+		}
+		if *keyFile == "" {
+			*keyFile = testdata.Path("server1.key")
+		}
+		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+		if err != nil {
+			log.Fatalf("Failed to generate credentials %v", err)
+		}
+		opts = []grpc.ServerOption{grpc.Creds(creds)}
+	}
+	grpcServer := grpc.NewServer(opts...)
+
 	http.Handle("/metrics", promhttp.Handler())
-	go http.ListenAndServe(":2112", nil)
+	go http.ListenAndServe(fmt.Sprintf("%s:%d", *metrics_ip, *metrics_port), nil)
 
 	csServer, err := server.NewCartServiceServer(*db_username, *db_password, *db_ip, *db_port, *db_name)
 	defer csServer.Close()
 
-	grpcServer := grpc.NewServer()
-
 	pb.RegisterCartServiceServer(grpcServer, csServer)
 	// ... // determine whether to use TLS
-	log.Printf("Starting server listening on 127.0.0.1:%d", *port)
+	log.Printf("Starting server listening on %s:%d", *ip, *port)
 	grpcServer.Serve(lis)
 }
